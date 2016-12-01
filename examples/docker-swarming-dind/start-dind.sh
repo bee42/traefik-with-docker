0de7ac7dfe9fa040e5242a5d1837d3a94ac9381d
Peter Rossbach <peterrossbach@me.com>
2016-12-01 20:41:32 +0100

    add stop dind script

--- examples/docker-swarming-dind/start-dind.sh
+++ examples/docker-swarming-dind/start-dind.sh
@@ -11,12 +11,14 @@ if [ ! -z "$1" ] ; then
 fi
 
 for WORKER_NUMBER in $(seq ${NUM_WORKERS}); do
-    docker run -d --privileged --name worker-${WORKER_NUMBER} --hostname=worker-${WORKER_NUMBER} -p ${WORKER_NUMBER}2375:2375 docker:1.13.0-rc2-dind
+    docker run -d --privileged --name worker-${WORKER_NUMBER} \
+      --hostname=worker-${WORKER_NUMBER} \
+      -p ${WORKER_NUMBER}2375:2375 docker:1.13.0-rc2-dind
     docker --host=localhost:${WORKER_NUMBER}2375 swarm join \
-    --token ${SWARM_TOKEN} \
-    ${SWARM_MASTER}:2377
+      --token ${SWARM_TOKEN} \
+      ${SWARM_MASTER}:2377
 done
 
-docker run -it -d -p 5080:8080 \
+docker run -it -d -p 5080:8080 --name visualizer  \
 -v /var/run/docker.sock:/var/run/docker.sock \
 manomarks/visualizer
---
+++ examples/docker-swarming-dind/stop-dind.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e -x
+
+: NUM_WORKERS=${NUM_WORKERS:=3}
+
+if [ ! -z "$1" ] ; then
+  NUM_WORKERS=$1
+fi
+
+for WORKER_NUMBER in $(seq ${NUM_WORKERS}); do
+    docker exec -ti worker-${WORKER_NUMBER} docker swarm leave 
+    docker rm -f --name worker-${WORKER_NUMBER}
+done
+
+docker rm -f visualizer
+docker swarm leave -f
