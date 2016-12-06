7c1e495aa62137991911859e4f098701d0b03b66
Peter Rossbach <peterrossbach@me.com>
2016-12-06 17:29:42 +0100

    start at manager

--- examples/prometheus/start-prom.sh
+++ examples/prometheus/start-prom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-docker network create --driver=overlay prometheus 
+docker network create --driver=overlay prometheus
 docker service create --network prom --name prometheus \
-       --publish 9090:9090 127.0.0.1:5000/prometheus
-
+  --constraint=node.role==manager \
+  --publish 9090:9090 127.0.0.1:5000/prometheus
