5b785d7a5867f2359a1d738f4502d5562af3dfac
Niclas Mietz <github@mietz.io>
2016-12-14 10:31:16 +0100

    Update start-dind.sh

--- examples/docker-swarming-dind/start-dind.sh
+++ examples/docker-swarming-dind/start-dind.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 set -e -x
 
-docker swarm init
+docker swarm init $1
 SWARM_TOKEN=$(docker swarm join-token -q worker)
 SWARM_MASTER=$(docker info | grep -w 'Node Address' | awk '{print $3}')
 : NUM_WORKERS=${NUM_WORKERS:=3}
 
-if [ ! -z "$1" ] ; then
-  NUM_WORKERS=$1
+if [ ! -z "$2" ] ; then
+  NUM_WORKERS=$2
 fi
 
 for WORKER_NUMBER in $(seq ${NUM_WORKERS}); do
     docker run -d --privileged --name worker-${WORKER_NUMBER} \
       --hostname=worker-${WORKER_NUMBER} \
       -p ${WORKER_NUMBER}2375:2375 docker:1.13.0-rc2-dind
-    docker --host=127.0.0.1:${WORKER_NUMBER}2375 swarm join \
+    sleep 2
+    docker --host=0.0.0.1:${WORKER_NUMBER}2375 swarm join \
       --token ${SWARM_TOKEN} \
       ${SWARM_MASTER}:2377
 done
