5aa96c674865f7153d137b5e3734e5eec59eec3f
Peter Rossbach <peterrossbach@me.com>
2017-01-24 10:57:29 +0100

    Update(dind): add local registry mirror and update to dind 1.13
    Signed-off-by: Peter Rossbach <peterrossbach@me.com>

--- examples/docker-swarming-dind/start-worker.sh
+++ examples/docker-swarming-dind/start-worker.sh
@@ -14,7 +14,12 @@ SWARM_MASTER=$(docker info | grep -w 'Node Address' | awk '{print $3}')
 
 docker run -d --privileged --name worker-${WORKER_NUMBER} \
   --hostname=worker-${WORKER_NUMBER} \
-  -p ${WORKER_NUMBER}2375:2375 docker:1.13.0-rc2-dind
+  -p ${WORKER_NUMBER}2375:2375 \
+  docker:1.13.0-dind \
+  --registry-mirror http://127.0.0.1:5001
+
+sleep 2
+
 docker --host=localhost:${WORKER_NUMBER}2375 swarm join \
   --token ${SWARM_TOKEN} \
   ${SWARM_MASTER}:2377
