145d628b1dba6eb0ba1a5ce1a3539e90cb25e9a3
Peter Rossbach <peterrossbach@me.com>
2017-01-29 08:09:52 +0100

    Fix: Formatting
    Signed-off-by: Peter Rossbach <peterrossbach@me.com>

--- examples/docker-swarming-dind/docker-swarm-gui.sh
+++ examples/docker-swarming-dind/docker-swarm-gui.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 set -e
-docker run -it -d -p 5090:8080 -v /var/run/docker.sock:/var/run/docker.sock  julienbreux/docker-swarm-gui:latest
-
+docker run -it -d -p 5090:8080 \
+  -v /var/run/docker.sock:/var/run/docker.sock  \
+  julienbreux/docker-swarm-gui:latest
