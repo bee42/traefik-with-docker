337f57a752f0212554c35c40986c6bc541dda549
Peter Rossbach <peterrossbach@me.com>
2017-02-03 15:13:37 +0100

    Fix and rename
    Signed-off-by: Peter Rossbach <peterrossbach@me.com>

--- examples/docker-swarming-dind/start-portainer.sh
+++ examples/docker-swarming-dind/start-portainer.sh
@@ -2,7 +2,7 @@
 
 set -e -x
 
-if [ "$(docker ps --filter name=portainer -q)" ];then
+if [ ! "$(docker ps --filter name=portainer -q)" ];then
   docker run -d -p 9000:9000 \
     --name portainer \
     -v /var/run/docker.sock:/var/run/docker.sock  \
