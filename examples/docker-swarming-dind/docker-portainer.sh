d4df105a8371174e15bbe9f529b6c1c604ec9cf4
Peter Rossbach <peterrossbach@me.com>
2017-02-03 15:08:55 +0100

    Add: portainer
    Signed-off-by: Peter Rossbach <peterrossbach@me.com>

---
+++ examples/docker-swarming-dind/docker-portainer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e -x
+
+if [ "$(docker ps --filter name=portainer -q)" ];then
+  docker run -d -p 9000:9000 \
+    --name portainer \
+    -v /var/run/docker.sock:/var/run/docker.sock  \
+   portainer/portainer
+fi
