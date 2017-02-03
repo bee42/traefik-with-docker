2dc1d4a64a5d41243f7652b52394eef5f8fa7f53
Peter Rossbach <peterrossbach@me.com>
2017-02-03 14:43:43 +0100

    Fix
    Signed-off-by: Peter Rossbach <peterrossbach@me.com>

--- examples/prometheus/start-whoami.sh
+++ examples/prometheus/start-whoami.sh
@@ -9,5 +9,5 @@ if [ ! "$(docker service ls --filter name=whoami -q)" ];then
     --name whoami \
     --label traefik.port=80 \
     --network $network \
-    emilevauge/whoami
+      emilevauge/whoami
 fi
