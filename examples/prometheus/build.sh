5c7ee3c057362bef44a67a31be5ca3eb30eb1706
Peter Rossbach <peterrossbach@me.com>
2017-02-08 08:56:31 +0100

    Add: prometheus as docker swarming services
    Signed-off-by: Peter Rossbach <peterrossbach@me.com>

--- examples/prometheus/build.sh
+++ examples/prometheus/build.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+IMAGE=prometheus
+ACCOUNT=127.0.0.1:5000/bee42
+TAG_LONG=0.1.0
 
-docker build -t 127.0.0.1:5000/prometheus .
-docker push 127.0.0.1:5000/prometheus
+DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
+
+docker build \
+ -f Dockerfile \
+ --build-arg BUILD_DATE=$DATE \
+ --build-arg VCS_REF=`git rev-parse --short HEAD` \
+ --build-arg VERSION=$(git describe --tags) \
+ --build-arg VCS_URL=$(git config --get remote.origin.url) \
+ --build-arg VCS_BRANCH=$(git rev-parse --abbrev-ref HEAD) \
+ -t="${ACCOUNT}/$IMAGE" \
+ -t="${ACCOUNT}/$IMAGE-$DATE" \
+ -t="${ACCOUNT}/$IMAGE-$TAG_LONG" \
+ .
