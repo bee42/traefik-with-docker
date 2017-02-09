dd6aff13d93efe50b0fe121b840fcbdb887051a1
Peter Rossbach <peterrossbach@me.com>
2017-02-09 22:16:32 +0100

    Fix: simple bugs for first build
    Signed-off-by: Peter Rossbach <peterrossbach@me.com>

--- examples/prometheus/build.sh
+++ examples/prometheus/build.sh
@@ -3,16 +3,17 @@ IMAGE=prometheus
 ACCOUNT=127.0.0.1:5000/bee42
 TAG_LONG=0.1.0
 
-DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
+BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
+DATE=`date -u +"%Y-%m-%dT%H-%M-%SZ"`
 
 docker build \
  -f Dockerfile \
- --build-arg BUILD_DATE=$DATE \
+ --build-arg BUILD_DATE=$BUILD_DATE \
  --build-arg VCS_REF=`git rev-parse --short HEAD` \
  --build-arg VERSION=$(git describe --tags) \
  --build-arg VCS_URL=$(git config --get remote.origin.url) \
  --build-arg VCS_BRANCH=$(git rev-parse --abbrev-ref HEAD) \
  -t="${ACCOUNT}/$IMAGE" \
- -t="${ACCOUNT}/$IMAGE-$DATE" \
- -t="${ACCOUNT}/$IMAGE-$TAG_LONG" \
+ -t="${ACCOUNT}/$IMAGE:$DATE" \
+ -t="${ACCOUNT}/$IMAGE:$TAG_LONG" \
  .
--- examples/prometheus/start-prom.sh
+++ examples/prometheus/start-prom.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-network=prometheus
-if [ ! "$(docker network ls --filter name=$network -q)" ];then
+: NETWORK=${NETWORK:=prometheus}
+if [ ! "$(docker network ls --filter name=$NETWORK -q)" ];then
   docker network create --driver=overlay --attachable $network
 fi
 
 if [ ! "$(docker service ls --filter name=prometheus -q)" ];then
-  docker service create --network $network --name prometheus \
+  docker service create --network $NETWORK --name prometheus \
     --constraint=node.role==manager \
     --publish 9090:9090 \
-    127.0.0.1:5000/prometheus
+    127.0.0.1:5000/bee42/prometheus
 fi
