0f66ecc689d75d2a7785439e3480081064781900
Peter Rossbach <peterrossbach@me.com>
2017-02-19 11:51:09 +0100

    Fix: Wait after start drill service
    Signed-off-by: Peter Rossbach <peterrossbach@me.com>

--- examples/drill/drill.sh
+++ examples/drill/drill.sh
@@ -8,6 +8,7 @@ fi
 if [ ! $(docker ps -q --filter label=com.docker.swarm.service.name=debug-$NETWORK) ];then
   docker service create --network $NETWORK --name debug-$NETWORK --mode global \
        127.0.0.1:5000/bee42/drill sleep 1000000000
+  sleep 2
 fi
 
 CID=$(docker ps -q --filter label=com.docker.swarm.service.name=debug-$NETWORK)
