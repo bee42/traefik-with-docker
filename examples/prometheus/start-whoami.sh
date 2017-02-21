ede1aa1764d930cfaf42cfe7194294ea5b4b6e5f
Peter Rossbach <peterrossbach@me.com>
2017-02-21 08:59:13 +0100

    Update: add traefik.enable=true label
    Signed-off-by: Peter Rossbach <peterrossbach@me.com>

--- examples/prometheus/start-whoami.sh
+++ examples/prometheus/start-whoami.sh
@@ -8,6 +8,7 @@ if [ ! "$(docker service ls --filter name=whoami -q)" ];then
   docker service create \
     --name whoami \
     --label traefik.port=80 \
+    --label traefik.enable=true \
     --network $network \
       emilevauge/whoami
 fi
